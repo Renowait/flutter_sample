e73204349dc18bef2bbc13294d069d5e4e992391
Renowait <3651051541147@rmutr.ac.th>
2024-12-02 11:42:06 +0700

    second commit

---
+++ flutter_sample/lib/my_container.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class MyContainer extends StatelessWidget {
+  const MyContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:  Text('Using Container'),
+        backgroundColor:  Color(0xFF455A64),
+        centerTitle: true,
+      ),
+      body:  Center(child: Column(
+        children: [
+          container1(),
+          container2(),
+        ],
+      )),
+    );
+  }//build
+
+  Widget container1() => Container(
+        height: 200,
+        margin:const EdgeInsets.all(20),
+        color: const Color(0xFFB0BEC5),
+        alignment: Alignment.center,
+        child:  const Text('สินค้าทั้งหมด',
+                style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+
+  );
+
+  Widget container2() => Container(
+        height: 200, width: 150,
+        margin: EdgeInsets.all(20),
+        padding: EdgeInsets.only(top: 20),
+        alignment: Alignment.center,
+        decoration:  BoxDecoration(
+          color: Colors.blue,
+          border: Border.all(
+            color: Colors.green,
+            style: BorderStyle.solid,
+            width: 10
+          ),
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.all(Radius.elliptical(50, 50))
+        ),
+        child: Text('สินค้าคงเหลือ',
+                style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+  );
+}
